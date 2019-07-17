6ac2c4116cb0ad57852b7ae462f660c38926c6dd
kranthi1355 <37641030+kranthi1355@users.noreply.github.com>
2019-07-16 21:22:31 -0600

    Initial commit

---
+++ src/main/java/com/springframework/docker/springbootdockerdemo/SpringbootdockerdemoApplication.java
@@ -0,0 +1,12 @@
+package com.springframework.docker.springbootdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdockerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdockerdemoApplication.class, args);
+	}
+}
